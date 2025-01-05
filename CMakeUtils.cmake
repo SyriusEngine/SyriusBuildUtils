e92c461849817aef89046f9185dc5008d8c8b204
artick788 <60552965+artick788@users.noreply.github.com>
2025-01-05 21:01:57 +0100

    Added function that includes libraries locally, or clones them from git

--- CMakeUtils.cmake
+++ CMakeUtils.cmake
@@ -1,3 +1,30 @@
+include(FetchContent)
+
 include(${CMAKE_CURRENT_LIST_DIR}/CMakeColoredText.cmake)
 
 set (CMAKE_CXX_STANDARD 17)
+
+function(INCLUDE_OR_FETCH LIB_NAME)
+    if (NOT TARGET ${LIB_NAME})
+        message(STATUS "Including ${LIB_NAME}")
+        # Assume the local version is in a child folder and that the name of the library is the same as the folder name
+        set(LIB_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/../${LIB_NAME})
+
+        # Locally available?
+        if (EXISTS ${LIB_ROOT})
+            message(STATUS "Using local version of ${LIB_NAME}")
+            add_subdirectory(${LIB_ROOT} ${CMAKE_BINARY_DIR}/${LIB_NAME})
+
+        else()
+            message(STATUS "Cloning ${LIB_NAME} from git (main branch)")
+            FetchContent_Declare(
+                ${LIB_NAME}
+                GIT_REPOSITORY https://github.com/SyriusEngine/${LIB_NAME}.git
+                GIT_SHALLOW TRUE
+                GIT_PROGRESS TRUE
+            )
+            FetchContent_MakeAvailable(${LIB_ROOT})
+        endif()
+    endif()
+endfunction()
+
