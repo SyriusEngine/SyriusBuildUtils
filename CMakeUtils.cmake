873e7aebd8b7754d27b4dfd9f2921da2a5245ce1
arthur <arthur.preal@atlascopco.com>
2025-01-13 16:36:19 +0100

    reworked how GTest is fetched, this is now done using FetchContent rather than a separate execute_process call

--- CMakeUtils.cmake
+++ CMakeUtils.cmake
@@ -33,3 +33,19 @@ function(INCLUDE_OR_FETCH LIB_NAME OUTPUT_LIB_ROOT)
     endif()
 endfunction()
 
+function(FETCH_GTEST)
+    if (NOT TARGET gtest)
+        message(STATUS "Fetching GTest")
+        FetchContent_Declare(
+            googletest
+            GIT_REPOSITORY https://github.com/google/googletest.git
+            GIT_TAG main
+            GIT_PROGRESS TRUE # Show progress
+            GIT_SHALLOW TRUE # Only fetch the latest commit
+        )
+        FetchContent_MakeAvailable(googletest)
+    else()
+        message(WARNING "No target gtest found!")
+    endif()
+endfunction()
+
