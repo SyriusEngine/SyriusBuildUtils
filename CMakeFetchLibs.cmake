67ceda61438a28e37efd174864fe82653be9ff06
artick788 <arthur.preal@gmail.com>
2025-04-03 13:19:17 +0200

    Improved library fetching

--- CMakeFetchLibs.cmake
+++ CMakeFetchLibs.cmake
@@ -1,48 +1,29 @@
 include(FetchContent)
 
-function(fetch_gtest)
-    if (NOT TARGET gtest)
-        message(STATUS "Fetching GTest")
+function(fetch_lib target_name url git_tag)
+    if (NOT TARGET ${target_name})
+        message(STATUS "Fetching ${target_name}")
         FetchContent_Declare(
-            googletest
-            GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG main
+            ${target_name}
+            GIT_REPOSITORY ${url}
+            GIT_TAG ${git_tag}
             GIT_PROGRESS TRUE # Show progress
             GIT_SHALLOW TRUE # Only fetch the latest commit
         )
-        FetchContent_MakeAvailable(googletest)
+        FetchContent_MakeAvailable(${target_name})
     else()
-        message(WARNING "No target gtest found!")
-    endif()
+        message(WARNING "No target ${target_name} found!")
+    endif ()
+endfunction()
+
+function(fetch_gtest)
+    fetch_lib(gtest, https://github.com/google/googletest.git, main)
 endfunction()
 
 function(fetch_glfw)
-    if (NOT TARGET glfw)
-        message(STATUS "Fetching GLFW")
-        FetchContent_Declare(
-            glfw
-            GIT_REPOSITORY https://github.com/glfw/glfw.git
-            GIT_TAG master
-            GIT_PROGRESS TRUE # Show progress
-            GIT_SHALLOW TRUE
-        )
-        FetchContent_MakeAvailable(glfw)
-    else()
-        message(WARNING "No target glfw found!")
-    endif()
+    fetch_lib(glfw, https://github.com/glfw/glfw.git, master)
 endfunction()
 
 function(fetch_glm)
-    if (NOT TARGET glm)
-        FetchContent_Declare(
-            glm
-            GIT_REPOSITORY	https://github.com/g-truc/glm.git
-            GIT_TAG 	bf71a834948186f4097caa076cd2663c69a10e1e #refs/tags/1.0.1
-            GIT_PROGRESS TRUE
-            GIT_SHALLOW TRUE
-        )
-        FetchContent_MakeAvailable(glm)
-    else()
-        message(WARNING "No target glm found!")
-    endif()
+    fetch_lib(glm, https://github.com/g-truc/glm.git, bf71a834948186f4097caa076cd2663c69a10e1e)
 endfunction()
