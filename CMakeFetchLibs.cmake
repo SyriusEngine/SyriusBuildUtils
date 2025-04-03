e09644a4ca62f26e4863c0dd82fa59e733f734ae
artick788 <arthur.preal@gmail.com>
2025-04-03 13:28:32 +0200

    Improved library fetching

--- CMakeFetchLibs.cmake
+++ CMakeFetchLibs.cmake
@@ -17,13 +17,13 @@ function(fetch_lib target_name url git_tag)
 endfunction()
 
 function(fetch_gtest)
-    fetch_lib(gtest, https://github.com/google/googletest.git, main)
+    fetch_lib(gtest https://github.com/google/googletest.git main)
 endfunction()
 
 function(fetch_glfw)
-    fetch_lib(glfw, https://github.com/glfw/glfw.git, master)
+    fetch_lib(glfw https://github.com/glfw/glfw.git master)
 endfunction()
 
 function(fetch_glm)
-    fetch_lib(glm, https://github.com/g-truc/glm.git, bf71a834948186f4097caa076cd2663c69a10e1e)
+    fetch_lib(glm https://github.com/g-truc/glm.git bf71a834948186f4097caa076cd2663c69a10e1e)
 endfunction()
